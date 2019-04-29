52bd06059045e88f3011f0a418e40fcd3bd3e922
Those45Ninjas <tom.parker1235@gmail.com>
2019-04-29 22:48:11 +0800

    Made GET /api/v1/shifts work

--- main.go
+++ main.go
@@ -1,23 +1,68 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"timesheets/timesheets"
 
+	"github.com/go-chi/render"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	shift := new Shift()
-	
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+// Routes sets up all the routes for this api.
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON), // Set the content type to application/json.
+		middleware.Logger,          // Loog all the API calls.
+		middleware.DefaultCompress, // Add some gzip to that sweet sweet json.
+		middleware.RedirectSlashes, // Tralling slashes are anoying, just make it work.
+		middleware.Recoverer,       // Recover from panics without crashing the server.
+	)
+
+	router.Route("/api/v1", func(r chi.Router) {
+		r.Mount("/shifts", timesheets.Routes())
 	})
-	http.ListenAndServe(":3333", r)
+
+	return router
 }
 
-// SELECT s.start, s.finish, TIMESTAMPDIFF(MINUTE, s.start, s.finish) / 60 AS total from shifts s;
+func main() {
+
+	// Setup the database and test it's connection.
+	db, err := sql.Open("mysql", "timesheets:uERv3dKu5643qjUN@/timesheets?parseTime=true")
+
+	if err != nil {
+		log.Panicf("Unable to open the database, err: %s\n", err.Error())
+	}
+	defer db.Close() // Close the database when the main function ends.
+
+	err = db.Ping() // Ping the database to test the username and password.
+	if err != nil {
+		log.Panicf("Unable to establish a connection to the database, err: %s\n", err.Error())
+	}
+
+	log.Print("Connected to the database successfully")
+
+	timesheets.Init(db)
+
+	// Setup the http API.
+	router := Routes()
+
+	// Dump each route into the console.
+	// TODO: Put this under a flag or debug level.
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route)
+		return nil
+	}
+
+	if err := chi.Walk(router, walkFunc); err != nil {
+		log.Panicf("Unable to walk the api, err: %s\n", err.Error())
+	}
+
+	log.Fatal(http.ListenAndServe(":8081", router)) // Start the http server.
+}
--- timesheets/shift.go
+++ timesheets/shift.go
@@ -1,20 +1,33 @@
 package timesheets
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
 )
 
+var (
+	db *sql.DB
+)
+
 // Shift stores details about a shift.
 type Shift struct {
-	ID     uint    `json:"id"`
-	Start  uint    `json:"start"`
-	Finish uint    `json:"finish"`
-	Total  float32 `json:"total"`
+	ID     uint      `json:"id"`
+	Start  time.Time `json:"start"`
+	Finish time.Time `json:"finish"`
+	Total  float32   `json:"total"`
 }
 
+// Init initializes.
+func Init(dbc *sql.DB) {
+	db = dbc
+}
+
+// Routes retruns all the routes for this module.
 func Routes() *chi.Mux {
 	router := chi.NewRouter()
 
@@ -61,5 +74,28 @@ func UpdateShift(w http.ResponseWriter, r *http.Request) {
 
 // GetAllShifts will get all the shifts in the database for the current user.
 func GetAllShifts(w http.ResponseWriter, r *http.Request) {
+	// Query the database.
+	rows, err := db.Query("SELECT s.id, s.start, s.finish, TIMESTAMPDIFF(MINUTE, s.start, s.finish) / 60 AS total from shifts s")
+	defer rows.Close()
 
+	if err != nil {
+		log.Fatalf("Error getting all shifts, %s", err.Error())
+	}
+
+	// put all the shifts into an array.
+	var shifts []Shift
+	for rows.Next() {
+		shift := Shift{}
+
+		err = rows.Scan(&shift.ID, &shift.Start, &shift.Finish, &shift.Total)
+
+		if err != nil {
+			panic(err)
+		}
+
+		shifts = append(shifts, shift)
+	}
+
+	// Dump that array to the client.
+	render.JSON(w, r, shifts)
 }
